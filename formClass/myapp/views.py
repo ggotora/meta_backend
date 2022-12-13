38887a2182ffa8a9f86f66e6dd37767117045252
gilbertgotora2@gmail.com <gilbertgotora2@gmail.com>
2022-12-13 06:18:20 +0200

    add forms.py and Myform instance to view and template

--- formClass/myapp/views.py
+++ formClass/myapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .forms import MyForm
 
 # Create your views here.
+def index(request):
+    form = MyForm()
+    return render(request, "myapp/index.html", {'form':form})
