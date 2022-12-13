63028dbea8897dc308e6478a268d6a99ad672f56
gilbertgotora2@gmail.com <gilbertgotora2@gmail.com>
2022-12-13 06:42:12 +0200

    Add different form_class fields and options

--- formClass/myapp/forms.py
+++ formClass/myapp/forms.py
@@ -1,4 +1,21 @@
 from django import forms 
 
 class MyForm(forms.Form):
-    name = forms.CharField()
+    #CharFiels
+    #EmailField
+    # IntegerField 
+    # MultipleChoice
+    # FileField
+
+    #Arguments (required, label, help_text, initial )
+    FAVORITE_FOODS = [ 
+        ("italian", "Italian"),
+        ("greek", "Greek"),
+        ("zimbo", "Zimbabwe"),
+    ]
+    name = forms.CharField(initial="John Doe")
+    story = forms.CharField(widget=forms.Textarea(attrs={"rows": 2}))
+    email = forms.EmailField()
+    reservation_date = forms.DateField(widget=forms.NumberInput(attrs={'type': 'date'}))
+    fav_dish = forms.ChoiceField(choices=FAVORITE_FOODS)
+    fav_dish_radio = forms.ChoiceField(choices=FAVORITE_FOODS, widget=forms.RadioSelect)
