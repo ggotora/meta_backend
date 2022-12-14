52ceccb303c8c6082e8be393e12fa55e891c3092
gilbertgotora2@gmail.com <gilbertgotora2@gmail.com>
2022-12-14 08:55:37 +0200

    Add Artist, Album and Song models

--- formClass/myapp/models.py
+++ formClass/myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model): 
+    name = models.CharField(max_length=10) 
+
+class Album(models.Model): 
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE) 
+
+class Song(models.Model): 
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE) 
+    album = models.ForeignKey(Album, on_delete=models.RESTRICT) 
